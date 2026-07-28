ac60d32f551ddaf57c462e281cb7d389e132890e
Aniket <aniketkrsingh12@gmail.com>
2026-07-28 21:48:00 +0530

    Configure deployment for Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     port: 3000,
     open: true
   }
 })
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/IBM-/",
-// });
